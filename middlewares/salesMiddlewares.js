0208e875189f788dd3a57635e5ac04250f514e7b
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-07-04 16:04:08 -0300

    erro de lint

--- middlewares/salesMiddlewares.js
+++ middlewares/salesMiddlewares.js
@@ -13,7 +13,7 @@ const quantitySales = (req, res, next) => {
     return res.status(400).json({ message: '"quantity" is required' });
   }
   const zero = 0;
-  if (quantity <= zero) {
+  if (quantity2 <= zero) {
     return res
       .status(422)
       .json({ message: '"quantity" must be greater than or equal to 1' });
